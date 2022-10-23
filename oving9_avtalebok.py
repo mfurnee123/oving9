897f300117293efa7ef9c1ac23057b82549f7577
Marte Bjerkreim Furnee <martefurnee@Martes-MBP.home>
2022-10-23 16:55:13 +0200

    begynt på ny avtale

--- oving9_avtalebok.py
+++ oving9_avtalebok.py
@@ -6,14 +6,35 @@ Created on Sun Oct 23 15:10:42 2022
 @author: martefurnee
 """
 
+from datetime import datetime
+
 class Avtale:
-    def __init__(self, tittel, sted, starttidspunkt, varighet):
+    def __init__(self, tittel=None, sted=None, starttidspunkt=None, varighet=None):
         self.tittel = tittel
         self.sted = sted
-        self.startttidspunkt = starttidspunkt
+        self.starttidspunkt = starttidspunkt
         self.varighet = varighet
-    
         
     def __str__(self):
         return f"Du har en avtale {self.tittel} kl.{self.starttidspunkt} som varer i {self.varighet}, sted: {self.sted}"
         
+    
+    def ny_avtale(self):
+        self.tittel = input("Tittel på avtalen: ")
+        self.sted = input("Sted: ")
+        self.starttidspunkt = input("Avtalen starter: ")
+        self.varighet = input("Avtalen varer: ")
+        return self.tittel, self.sted, self.starttidspunkt, self.varighet
+        
+        
+        
+        
+        
+        
+
+
+if __name__ == "__main__":   
+    avtale = Avtale()
+    avtale.ny_avtale()
+    print(avtale)
+    
