3fa50292ed41ce90c005db82b486c4cae2718a11
Marte Bjerkreim Furnee <martefurnee@Martes-MBP.home>
2022-10-23 15:40:58 +0200

    Laget klassen med init og str

--- oving9_avtalebok.py
+++ oving9_avtalebok.py
@@ -6,4 +6,14 @@ Created on Sun Oct 23 15:10:42 2022
 @author: martefurnee
 """
 
-class Avtale(self, )
+class Avtale:
+    def __init__(self, tittel, sted, starttidspunkt, varighet):
+        self.tittel = tittel
+        self.sted = sted
+        self.startttidspunkt = starttidspunkt
+        self.varighet = varighet
+    
+        
+    def __str__(self):
+        return f"Du har en avtale {self.tittel} kl.{self.starttidspunkt} som varer i {self.varighet}, sted: {self.sted}"
+        
