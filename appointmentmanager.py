c13fad702385d2b5061e66095d99323629659f78
Simen Eilevstjønn <simene@lyse.net>
2022-10-24 21:50:49 +0200

    Skeleton appointment manager class

---
+++ appointmentmanager.py
@@ -0,0 +1,33 @@
+from oving9_avtalebok import Avtale
+
+class AppointmentManager:
+    def __init__(self):
+        self.appointments = []
+
+    def newAppointment(self):
+        """
+        Creates a new appointment from user input from STDIN.
+        """
+
+        # The ny_avtale method is an instance method of the Avtale class. This is not intuitive, and should preferably be moved here. 
+        a = Avtale()
+
+        a.ny_avtale()
+
+        self.appointments.append(a)
+
+    def readAppointments(self):
+        raise NotImplementedError
+
+    def writeAppointments(self):
+        raise NotImplementedError
+
+    def listAppointments(self):
+        raise NotImplementedError
+    
+    def editAppointment(self):
+        raise NotImplementedError
+    
+    def deleteAppointment(self):
+        raise NotImplementedError
+
