2f976bc42636fafc2481f6b7a74e8e5b0d9b9757
Simen Eilevstjønn <simene@lyse.net>
2022-10-25 20:20:06 +0200

    #13 Method for getting an index from user input

--- appointmentmanager.py
+++ appointmentmanager.py
@@ -4,6 +4,33 @@ class AppointmentManager:
     def __init__(self):
         self.appointments = []
 
+    def __userIndex(self):
+        """
+        Gets an index for an appointment from user input from STDIN.
+
+        :rtype int:
+        :returns: An index within bounds for the self.appointments list.
+        """
+
+        # Loop until a valid input is received
+        idx = None
+
+        while idx == None:
+            raw = input(">")
+
+            try:
+                idx = int(raw)
+            except:
+                print("Feil, input må være et heltall.\n")
+                idx = None
+            
+            # Validate that the number is in the correct range
+            if not (0 <= idx < len(self.appointments)):
+                print(f"Feil, input må være et heltall mellom 0 og {len(self.appointments) - 1} inklusivt.\n")
+                idx = None
+
+        return idx
+
     def newAppointment(self):
         """
         Creates a new appointment from user input from STDIN.
